6b94571d5582477cde774ccbcf13f47540e9aa3b
hasaannn327-source <hasaannn327@gmail.com>
2025-07-26 16:40:06 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React dosyalarının olduğu klasörler
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}

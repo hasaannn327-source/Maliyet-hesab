d441ce98ee72d1568c3bb2b059f5a2ae04f8abae
hasaannn327-source <hasaannn327@gmail.com>
2025-08-03 16:33:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+@tailwind base;
+@tailwind components;
+@tailwind utilities;

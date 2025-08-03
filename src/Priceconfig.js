0ec979b4b3f00591b893d5d30ab74a08c0a85f84
hasaannn327-source <hasaannn327@gmail.com>
2025-08-03 16:39:50 +0300

    Create Priceconfig.js

---
+++ src/Priceconfig.js
@@ -0,0 +1,31 @@
+// Fiyat yapılandırma dosyası
+export const PRICES = {
+  beton: {
+    m3Fiyat: 3000,
+    m3PerM2: 0.35
+  },
+  demir: {
+    tonFiyat: 27000,
+    tonPerM2: 0.04
+  }
+};
+
+// Şehir çarpanları
+export const CITY_MULTIPLIERS = {
+  'İstanbul': 1.25,
+  'Ankara': 1.15,
+  'İzmir': 1.20,
+  'Antalya': 1.18,
+  'Bursa': 1.12,
+  'Adana': 1.08,
+  'Gaziantep': 1.05,
+  'Konya': 1.03,
+  'Diğer': 1.0
+};
+
+// Kalite çarpanları
+export const QUALITY_MULTIPLIERS = {
+  'Ekonomik': 0.85,
+  'Standart': 1.0,
+  'Lüks': 1.35
+};
